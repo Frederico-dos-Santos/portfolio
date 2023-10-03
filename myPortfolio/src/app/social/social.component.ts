c479cccfdd4477e87398fca4d46af1b8e52588a4
Frederico dos Santos <fredericoandrade7@gmail.com>
2023-10-02 22:18:16 -0300

    Update social.component.ts

--- myPortfolio/src/app/social/social.component.ts
+++ myPortfolio/src/app/social/social.component.ts
@@ -16,7 +16,7 @@ export class SocialComponent implements OnInit{
   social: iSocial[] = [];
 
   ngOnInit(): void {
-    this.social.push({name: 'WHATSAPP', link: 'https://w.app/oFrederico', img: 'assets/icons/wpp.svg'});
+    this.social.push({name: 'WHATSAPP', link: 'https://api.whatsapp.com/send/?phone=5531983730232&text&type=phone_number&app_absent=0', img: 'assets/icons/wpp.svg'});
     this.social.push({name: 'EMAIL', link: 'mailto:fredericoandrade7@gmail.com', img: 'assets/icons/email.svg'});
     this.social.push({name: 'GITHUB', link: 'https://github.com/Frederico-dos-Santos', img: 'assets/icons/github.png'});
     this.social.push({name: 'LINKEDIN', link: 'https://www.linkedin.com/in/frederico-dos-sant0s/', img: 'assets/icons/linkedin.svg'});
